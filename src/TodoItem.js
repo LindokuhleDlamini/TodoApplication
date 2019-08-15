7415a802770f4b47a634ae9a4a76016bdb86af56
Lindokuhle Dlamini <DLMLIN017@myuct.ac.za>
2019-08-15 12:46:05 +0200

    added the editTodo action dispatch in the handleTitleChange function

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -5,13 +5,14 @@ class TodoItem extends Component {
         super(props);
 
         this.state = {
+            id: this.props.todo.id,
             title: this.props.todo.title,
             completed: this.props.todo.completed,
         }
     }
 
     handleComplete() {
-        this.props.actions.completeTodo(this.props.todo.id);
+        this.props.actions.completeTodo(this.state.id);
 
         this.setState({
             completed: !this.state.completed
@@ -19,14 +20,15 @@ class TodoItem extends Component {
     }
 
     handleDelete() {
-        this.props.actions.deleteTodo(this.props.todo.id);
+        this.props.actions.deleteTodo(this.state.id);
     }
 
     handleTitleChange(event) {
         this.setState({
             title: event.target.value
-        })
+        });
 
+        this.props.actions.editTodo(this.state.id, this.state.title);
     }
 
     render() {
